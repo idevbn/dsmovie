09eb2afe7b13cde0e69ee5898a09b3fbda826a19
Idevaldo <inetomec@yahoo.com.br>
2022-05-13 12:41:32 -0300

    feat: :sparkles: Navigation Buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 function Form() {
@@ -11,7 +12,7 @@ function Form() {
 
   return (
     <div className="dsmovie-form-container">
-      <img className="dsmovie-movie-card-image" src={ movie.image } alt={ movie.title } />
+      <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
       <div className="dsmovie-card-bottom-container">
           <h3>{ movie.title }</h3>
           <form className="dsmovie-form">
@@ -32,8 +33,10 @@ function Form() {
               <div className="dsmovie-form-btn-container">
                   <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
               </div>
-          </form >
-          <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+          </form>
+          <Link to="/">
+            <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+          </Link>
       </div>
     </div>
   );
