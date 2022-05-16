4c680a7fb248221f9439640b2e76edade2d5891a
Idevaldo <inetomec@yahoo.com.br>
2022-05-15 22:16:56 -0300

    feat: :sparkles: Domain Model, Database Seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+
+    public User() {
+    }
+
+    public User(Long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+}
